95dd715ffb3aa4e6fbff821e480a7062bccf5751
Vik63 <sfoto116@yandex.ru>
2022-07-10 18:40:11 +0400

    15-5

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ app.post('/signup', celebrate({
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required().min(6),
   }),
 }), login);
 
